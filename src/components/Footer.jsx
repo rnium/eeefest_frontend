a6300584298eaebb52a3e1068899f839356efd32
rony <rniumo@gmail.com>
2024-02-25 23:11:40 +0600

    my email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import {
-    AppBar, Toolbar, Typography, Box, Grid
-} from '@mui/material'
+    Typography, Box
+} from '@mui/material';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import BugReportIcon from '@mui/icons-material/BugReport';
 
 const Footer = () => {
     return (
-        <Box sx={{py:4}} className="footer" display="flex" flexDirection="column" justifyContent="center" alignItems="center" >
-            <Box >
-                <img className='logo' src='static/images/sec_eee.png' />
+        <Box>
+            <Box sx={{ py: 4 }} className="footer" display="flex" flexDirection="column" justifyContent="center" alignItems="center" >
+                <Box >
+                    <img className='logo' src='static/images/sec_eee.png' />
+                </Box>
+                <Typography variant='h6' fontSize={{ xs: '0.9rem', md: '1.3rem' }} textAlign='center'>TechnoVenture 3.0 presented by EEE Association</Typography>
+                <Typography variant='body1' fontSize={{ xs: '0.7rem', md: '1.1rem' }} className='text-muted' component="div">&copy; Department of EEE, Sylhet Engineering College.</Typography>
             </Box>
-            <Typography variant='h6' fontSize={{xs: '0.9rem', md: '1.3rem'}} textAlign='center'>TechnoVenture 3.0 presented by EEE Association</Typography>
-            <Typography variant='body1' fontSize={{xs: '0.7rem', md: '1.1rem'}} className='text-muted' component="div">&copy; Department of EEE, Sylhet Engineering College.</Typography>
+            <Box sx={{py: '2px', marginLeft: 1}} style={{ backgroundColor: 'white' }} display="flex" flexDirection="row" justifyContent="center" alignContent="center">
+                <BugReportIcon color='tertiary' />
+                <Typography variant='body1' alignSelf="center" fontSize={{ xs: '0.6rem', md: '0.8rem' }} sx={{textAlign: 'center', marginLeft: 1}} component="span">For any site related issues, contact: <a href="mailto:ronyeee@sec.ac.bd">ronyeee@sec.ac.bd</a></Typography>
+            </Box>
         </Box>
     )
 }
